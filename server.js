4622cca96a2d8fa9c46d2a99625bb7864013d871
PARTHKHUNTETA <parthkhunteta96@gmail.com>
2017-08-19 19:21:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var content=
+{
+    title:'article one |parth khunteta',
+    heading:'article one',
+    date:'19 august 2017',
+    content:
+    <p>
+                this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article 
+            </p>
+            <p>
+                  this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article 
+            </p>
+            <p>
+                this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article this is the content for  my first article 
+            </p>'
+            
+
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
