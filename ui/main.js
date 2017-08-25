93aa608ab95bc014ba20d82da037e940c24a2673
PARTHKHUNTETA <parthkhunteta96@gmail.com>
2017-08-25 12:44:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element=document.getElementById('main-text');
 element.innerHTML="new value";
 var image=document.getElementById('img 0');
 img.onclick=function(){
-  img.style.marginLeft='100px'  
+  img.style.marginLeft='100px'; 
 };
