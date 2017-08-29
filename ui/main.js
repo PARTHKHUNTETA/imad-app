8d109375d736387848d0a1188f2e21eee943ac61
Parth Khunteta <parthkhunteta96@gmail.com>
2017-08-29 23:05:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,10 +20,26 @@ button.onclick = function()
         }
       
   };
-  
-  
-  //make the request
   request.open('GET','http://parthkhunteta96.imad.hasura-app.io/counter',true);
   request.send(null);
 
+};
+var nameInput= document.getElementById('name');
+var name= nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick=function()
+{
+  
+  
+  var names=['name 1','name 2','name 3'];
+  var list ='';
+  for(var i=0;i<names.length;i++)
+  {
+      list+='<li>'+  names[i] + '</li>';
+      
+  }
+    var ul= document.getElementById('namelist');
+    ul.innerHTML=list;
+    
+    
 };
