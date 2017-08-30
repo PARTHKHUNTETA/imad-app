7a3c18ae62f6da2b607fc8ac8382b205b20ca6ef
Parth Khunteta <parthkhunteta96@gmail.com>
2017-08-30 19:41:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,7 @@ button.onclick = function()
   request.send(null);
 
 };
-var nameInput= document.getElementById('name');
-var name= nameInput.value;
+
 var submit= document.getElementById('submit_btn');
 submit.onclick=function()
 {
@@ -57,6 +56,8 @@ submit.onclick=function()
         }
       
   };
+  var nameInput= document.getElementById('name');
+   var name= nameInput.value;
   request.open('GET','http://parthkhunteta96.imad.hasura-app.io/submit-name?=name' + name,true);
   request.send(null);
 };
