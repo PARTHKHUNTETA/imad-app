3b21ecda5544c714d13d30bb969a62ee7c9e0a71
Parth Khunteta <parthkhunteta96@gmail.com>
2017-08-30 19:30:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,17 +29,34 @@ var name= nameInput.value;
 var submit= document.getElementById('submit_btn');
 submit.onclick=function()
 {
+
+     var request= new XMLHttpRequest();
   
-  
-  var names=['name 1','name 2','name 3','name 4'];
-  var list ='';
-  for(var i=0;i<names.length;i++)
+  //capture the response and store it in a variable
+  request.onreadystatechange=function()
   {
-      list+='<li>'+  names[i] + '</li>';
+      if (request.readystate==XMLHttpRequest.DONE)
+        {     if(request.status==200)
+            {
+                      
+                        var names=request.responseText;
+                        names.JSON.parse(names);
+                        var list ='';
+                        for(var i=0;i<names.length;i++)
+                        {
+                            list+='<li>'+  names[i] + '</li>';
       
-  }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML=list;
-    
-    
+                        }
+                        var ul= document.getElementById('namelist');
+                        ul.innerHTML=list;
+ 
+            
+            }
+  
+      
+        }
+      
+  };
+  request.open('GET','http://parthkhunteta96.imad.hasura-app.io/submit-name?=name' + name,true);
+  request.send(null);
 };
