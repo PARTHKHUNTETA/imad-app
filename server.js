ad29d900f0061381d8117b882c1583d074070241
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-05 01:07:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,12 +106,12 @@ var htmlTemplate=`
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
  var Pool=new Pool(config);
- app.get('/test-db',function(req,res)
- {
+ app.get('/test-db', function(req,res){
      
-    Pool.query('SELECT * FROM test',function(err,result)
-    {
+    Pool.query('SELECT * FROM test',function(err,result){
        if(err){
            
            res.ststus(500).send(err.toString());
