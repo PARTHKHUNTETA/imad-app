0f6af6c185e054d521af3822caebd4e6e5047cde
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-05 01:38:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.get('/articles/:articleName', function (req, res)
            }
           else
           {
-           var articleDATA=result.rows[0];
+           var articleData=result.rows[0];
            res.send(createTemplate(articleData));
            
               
