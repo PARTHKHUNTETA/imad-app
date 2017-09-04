1bceead8e2400ace5989d517459c6378df6bd845
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-05 01:01:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/', function (req, res) {
  app.get('/test-db',function(req,res)
  {
      
-    Pool.query('select * FROM test',function(err,result)
+    Pool.query('SELECT * FROM test',function(err,result)
     {
        if(err){
            
