388727028d473712277bc83fc2e4e01ac8bfcd38
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-05 01:43:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,12 +157,12 @@ app.get('/articles/:articleName', function (req, res)
            res.ststus(500).send(err.toString());
        } 
        else{
-           if(result.rows.length===0){
+           if(result.rows.length == 0){
            res.status(404).send('article not found');
            }
           else
           {
-           var articleData=result.rows[0];
+           var articleData = result.rows[0];
            res.send(createTemplate(articleData));
             }
            
