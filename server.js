1f3773127628b650f9589e6cd94e9d7558e3d5f2
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-05 01:02:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var pool=require('pg').Pool;
 
 var config = {
     user:'parthkhunteta96',
-    database:'parthkhuntetta96',
+    database:'parthkhunteta96',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
