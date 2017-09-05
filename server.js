ed8a25fbded28a5b6c3c7b56a81c2a1685d00648
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-05 22:28:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser=require('body-parser');
 
 var config = {
     user:'parthkhunteta96',
@@ -17,6 +18,9 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.jason());
+
+
 
 var articles={
 'article-one':{
@@ -118,6 +122,31 @@ app.get('/', function (req, res) {
  {
     var hashedString = hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
+    
+     
+ });
+ 
+ app.post('/create-user',function(req,res){
+     
+     var username =req.body.username;
+     var password=req.body.password;
+     
+     
+     var salt=crypto.getRandomBytes(128).toString('hex');
+     var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES($1, $2)', [username,dbString] ,function(err,result){
+         if(err){
+           
+           res.ststus(500).send(err.toString());
+       } 
+       else{
+           res.send('User successfully created :'+ username);
+       }
+       
+        
+        
+    });    
+    
     
      
  });
