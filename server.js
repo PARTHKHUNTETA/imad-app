57a8e03691ce9a2826e654db92a7662f541cc62b
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-06 22:32:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto = require('crypto');
-var bodyParser=require('body-parser');
-
+var session=require('express-session');
 var config = {
     user:'parthkhunteta96',
     database:'parthkhunteta96',
@@ -15,6 +14,14 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'someRandomSecretValue',
+    cookie:{maxAge:1000 * 60 * 60 * 24 * 30}
+}));
+
+
+
+
 var articles={
 'article-one':{
 
@@ -150,7 +157,10 @@ app.get('/', function (req, res) {
                         var salt = dbString.split('$')[2];
                         var hashedPassword = hash(password, salt);
                         
-                        if(hashedPassword===dbString){
+                        if(hashedPassword ===dbString){
+                            
+                        req.session.auth={userId:result.rows[0].id};   
+                            
                         res.send('credentials are correct!');
                         }
                         else
@@ -162,6 +172,15 @@ app.get('/', function (req, res) {
        });  
      
 });
+ 
+ app.get('/check-login',function(req,res){
+     
+ if(req.session && req.session.auth && req.session.auth.userId)
+ {
+     res.send('youare logged in: ' + req.session.auth.userId.toString());
+ }
+     
+ });
  
   var Pool = new Pool(config);
  app.get('/test-db', function(req,res){
