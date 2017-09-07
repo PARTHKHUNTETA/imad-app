a3e7853685b4454fbd1844cd17d3c97b5ae1d1c4
Parth Khunteta <parthkhunteta96@gmail.com>
2017-09-07 18:56:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,28 +9,26 @@ submit.onclick=function()
   request.onreadystatechange=function()
   {
       if (request.readystate==XMLHttpRequest.DONE)
-        {     if(request.status==200){ 
-                  console.log('user logged in');
-                    alert('logged in successfully');
+        {        if(request.status === 200)
+                    { 
+                  
+                                 alert('logged in successfully');
                     }
-                else if(request.status===403)
-                {
-                    alert('username/passsword is incorrect');
-                }
-                else if(request.status===500)
-                {
-                    alert('something went wrong on the server');
-                }
-  
-      
-        }
-      
-  };
+                else if(request.status === 403)
+                    {
+                                 alert('username/passsword is incorrect');
+                    }
+                else if(request.status === 500)
+                    {
+                                alert('something went wrong on the server');
+                    }
+       }
+    };
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   console.log(username);
   console.log(password);
-  request.open('POST','http://parthkhunteta96.imad.hasura-app.io/login',true);
+  request.open('POST',  'http://parthkhunteta96.imad.hasura-app.io/login', true);
   request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify({username:username,password:password}));
+  request.send(JSON.stringify({username : username, password : password}));
 };
